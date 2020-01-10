af906f78242c214fbdf7a952e327461b23e9cd60
Julia <juliabrain2020@u.northwestern.edu>
2020-01-10 00:31:14 -0600

    test commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hi team
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
