b801dc6c75c63ac8aacf29e29e1fdbdc17f1857c
Jidé Anene <dominicanene@gmail.com>
2020-01-15 20:10:51 -0600

    fixed firebaseConfigs to the new schema

--- src/updateDb/UpdateDb.js
+++ src/updateDb/UpdateDb.js
@@ -4,15 +4,16 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCH3KUTJ4R8YRDS6htIJrYORxiknxfd5dQ",
-    authDomain: "findatime-4fb7c.firebaseapp.com",
-    databaseURL: "https://findatime-4fb7c.firebaseio.com",
-    projectId: "findatime",
-    storageBucket: "findatime.appspot.com",
-    messagingSenderId: "361706564526",
-    appId: "1:361706564526:web:7e5f7bbfcc97dd023a763f",
-    measurementId: "G-QKQSXZLMYZ"
+    apiKey: "AIzaSyBb4_EJIIE5Cld3b8EUnJEki3XYd8wqdSM",
+    authDomain: "find-a-time-19756.firebaseapp.com",
+    databaseURL: "https://find-a-time-19756.firebaseio.com",
+    projectId: "find-a-time-19756",
+    storageBucket: "find-a-time-19756.appspot.com",
+    messagingSenderId: "990635372360",
+    appId: "1:990635372360:web:1c3b84e3ee6ce5fd084d7d",
+    measurementId: "G-F6QD99Q788"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // Get a reference to the database service
@@ -20,7 +21,7 @@ const db = firebase.database();
 
 
 const addFreeInterval = ({eventId, date, interval, userName}) => {
-    db.ref('events/' + eventId + "/data/" + date + "/" + interval + "/" + userName)
+    db.ref('rooms/' + eventId + "/data/" + date + "/" + interval + "/" + userName)
         .remove().then(() => {
         console.log("Remove succeeded.")
     })
@@ -30,7 +31,7 @@ const addFreeInterval = ({eventId, date, interval, userName}) => {
 };
 
 const addBusyInterval = ({eventId, date, interval, userName}) => {
-    db.ref('events/' + eventId + "/data/" + date + "/" + interval)
+    db.ref('rooms/' + eventId + "/data/" + date + "/" + interval)
         .child(userName)
         .set(1)
         .catch(error => alert(error));
@@ -46,6 +47,7 @@ const addBusyInterval = ({eventId, date, interval, userName}) => {
  * @example {"eventId": 1, "date": "2020-01-10", "interval": "09:30:00", "userName": "Dan", "isBusy": 1};
  */
 const UpdateDb = ({eventId, date, interval, userName, isBusy}) => {
+    console.log(eventId);
     if (isBusy) {
         addBusyInterval({eventId, date, interval, userName});
     } else {
