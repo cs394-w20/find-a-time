3160289da60331ec3dc338da816031f84fdec98c
Jidé Anene <dominicanene@gmail.com>
2020-01-17 15:57:54 -0600

    still broken

--- src/calendar/ScheduleEvents.js
+++ src/calendar/ScheduleEvents.js
@@ -1,53 +1,63 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import UpdateDb from "../Db/UpdateDb";
+
 var moment = require('moment');
 
 
-
 const hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
-const minutes = ['00', '30']
+const minutes = ['00', '30'];
 const dates = ['M', 'Tu', 'W', 'Th', 'F']; // replace this with the dates, try to get from gcal
 
 
 //eventId, date, interval, userName, isBusy
 const AddEvents = (eventId, userName, events) => {
+    console.log(events);
     events.map(event =>
-        findIntervals(eventId, userName, event))
-}
+        findIntervals(eventId, userName, event));
+    //findIntervals(eventId, userName, events[0]);
+
+};
+
+
+const getStartTime = (event) => {
+    var start = event.start.dateTime;
+    if (!start) {
+        start = event.start.date;
+    }
+    return moment(start);
+};
+
+const getEndTime = (event) => {
+    var end = event.end.dateTime;
+    if (!end) {
+        end = event.end.date;
+    }
+    return moment(end);
+};
+
+const getDay = (time) => {
+    time = time.toISOString().split('T', 1);
+    return time
+};
 
 const findIntervals = (eventId, userName, event) => {
     const start = getStartTime(event);
     const end = getEndTime(event);
     findBuckets(eventId, userName, start, end);
-}
+};
 
-const getStartTime = (event) => {
-    var start = event.start.dateTime;
-    if (!start){
-        start = event.start.date;
-    }
-    return moment(start);
-}
-
-const getEndTime = (event) => {
-    var end = event.end.dateTime;
-    if (!end){
-        end = event.end.date;
-    }    
-    return moment(end);
-}
-
-const getDay = (time) => {
-    time = time.toISOString().split('T', 1);
-    return time
-}
 
 const findBuckets = (eventId, userName, curr, end) => {
     const endMin = end.get('minute');
     const endHour = end.get('hour');
-    var i = 0;
-    while (i < minutes.length){
-        if (minutes[i] <= curr.get('minute') < minutes[i + 1]){
+    let i = 0;
+
+    console.log(Number(minutes[0]));
+    // 0,1
+
+    // Getting the first bucket -- figuring out what minute to start at
+    while (i < minutes.length - 1) {
+        if (Number(minutes[i]) <= curr.get('minute') && curr.get('minute') < Number(minutes[i + 1])) {
             break
         }
         i += 1;
@@ -56,21 +66,57 @@ const findBuckets = (eventId, userName, curr, end) => {
     //console.log("Finding intervals");
     let returnList = [];
     let payload;
-    while (!(curr.get('day') == end.get('day') && curr.get('hour') == endHour && minutes[i] > endMin)){
-       payload = {"eventId":eventId,"date":getDay(curr), "userName":userName, "interval":`${curr.get('hour')}:${minutes[i]}`, "isBusy": true };
+
+    // eslint-disable-next-line eqeqeq
+
+    //start: 6pm    end: 8pm , today
+    // i = 0
+    // i =1
+
+    // Keep  changing curr, and while curr != end{day, hr, min}
+    console.log("Herrrrrrrr");
+    console.log(curr.toString());
+    console.log(end.toString());
+    console.log(curr.get('date')); //wrong
+    console.log(getDay(curr)); //wrong
+
+
+
+    console.log(end.get('day')); //wrong
+    console.log(curr.get('hour'));
+    console.log(endHour);
+    console.log(Number(minutes[i]));
+    console.log(endMin);
+
+
+
+
+
+    while (!(getDay(curr) === getDay(end) && curr.get('hour') === endHour/* && Number(minutes[i]) > endMin*/)) {
+        payload = {
+            "eventId": eventId,
+            "date": getDay(curr),
+            "userName": userName,
+            "interval": `${curr.get('hour')}:${minutes[i]}`,
+            "isBusy": true
+        };
+
         returnList.push(payload);
         console.log("hey");
-       // UpdateDb(payload);
+        // UpdateDb(payload);
         i += 1;
-        if (i == minutes.length){
+        if (i === minutes.length) {
             curr.add(1, 'h');
             i = 0;
-        }        
+        }
     }
-    console.log("length of list: "+ returnList.length);
-    returnList.forEach((item)=>{ UpdateDb(item)});
+
+    console.log("length of list: " + returnList.length);
+    returnList.forEach((item) => {
+        UpdateDb(item)
+    });
 
 
-}
+};
 
 export default AddEvents;
