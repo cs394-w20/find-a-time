a2952c7da09e7bdad1fb90111af131cf2728ff66
Zack <szaslan@gmail.com>
2020-01-23 11:36:28 -0600

    Create custom button component

--- src/components/AuthButton/AuthButton.js
+++ src/components/AuthButton/AuthButton.js
@@ -1,18 +1,23 @@
 import React from "react"
+import { Button } from "../Button"
 import classnames from "classnames"
 import "./AuthButton.css"
 
 const AuthButton = ({ isAuthorized, handleAuthClick, handleSignoutClick }) => {
   const handleClick = isAuthorized ? handleSignoutClick : handleAuthClick
+  const title = isAuthorized
+    ? "Sign out of Google"
+    : "Sign in with Google Calendar"
   return (
-    <button
-      onClick={handleClick}
-      className={classnames("auth-button", {
-        "auth-button-secondary": isAuthorized
-      })}
-    >
-      {isAuthorized ? "Sign out of Google" : "Sign in with Google Calendar"}
-    </button>
+    <Button title={title} onClick={handleClick} className="auth-button" />
+    // <button
+    //   onClick={handleClick}
+    //   className={classnames("auth-button", {
+    //     "auth-button-secondary": isAuthorized
+    //   })}
+    // >
+    //   {isAuthorized ? "Sign out of Google" : "Sign in with Google Calendar"}
+    // </button>
   )
 }
 
---
+++ src/components/Button/Button.js
@@ -0,0 +1,25 @@
+import React from "react"
+import classnames from "classnames"
+import "./Button.css"
+
+const Button = ({ onClick, title, type, className, ...rest }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={classnames("a-button", {
+        [`a-button-${type}`]: type,
+        [className]: className
+      })}
+    >
+      {title}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  onClick: () => {},
+  type: "primary",
+  title: "I am a Button"
+}
+
+export default Button
