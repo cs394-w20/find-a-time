0ce258be8c57ed9f6e23fb25636797462f30df3f
jdanene <dominicanene@gmail.com>
2020-01-25 23:03:37 -0600

    Dummy_data and db schema

---
+++ public/dummy_data_json.js
@@ -0,0 +1,90 @@
+{
+  "rooms": {
+    "1": {
+      "data": {
+        "2020-01-10": {
+          "09:00": {
+            "julia@northwestern.edu": "MANUAL",
+            "suzy@northwestern.edu": "MANUAL"
+          },
+          "09:30": {
+            "jide@northwestern.edu": "AUTO"
+          },
+          "11:00": {
+            "julia@northwestern.edu": "AUTO",
+            "suzy@northwestern.edu": "AUTO"
+          },
+          "11:30": {
+            "julia@northwestern.edu": "AUTO",
+            "suzy@northwestern.edu": "AUTO"
+          }
+        },
+        "2020-01-11": {
+          "09:00": {
+            "julia@northwestern.edu": "AUTO",
+            "suzy@northwestern.edu": "AUTO"
+          },
+          "09:30": {
+            "jide@northwestern.edu": "AUTO"
+          },
+          "11:00": {
+            "julia@northwestern.edu": "AUTO",
+            "suzy@northwestern.edu": "MANUAL"
+          },
+          "11:30": {
+            "julia@northwestern.edu": "AUTO",
+            "suzy@northwestern.edu": "MANUAL"
+          }
+        },
+        "2020-01-12": {
+          "09:00": {
+            "julia@northwestern.edu": "AUTO",
+            "suzy@northwestern.edu": "AUTO"
+          },
+          "09:30": {
+            "jide@northwestern.edu": "AUTO"
+          },
+          "11:00": {
+            "julia@northwestern.edu": "AUTO",
+            "suzy@northwestern.edu": "AUTO"
+          },
+          "11:30": {
+            "julia@northwestern.edu": "AUTO",
+            "suzy@northwestern.edu": "AUTO"
+          }
+        }
+      },
+      "users": {
+        "julia@northwestern.edu": "Julia",
+        "jide@northwestern.edu": "Jide",
+        "suzy@northwestern.edu": "Suzy"
+      },
+      "time_interval": {
+        "start": "2020-01-10",
+        "end": "2020-01-12"
+      },
+      "meta_data": {
+        "title": "CS 394 Meeting",
+        "description": "Make sure you fill out this form so we can find a time to meet weekly!",
+        "room_owner": "suzy@northwestern.edu"
+      }
+    }
+  },
+  "users": {
+    "julia@northwestern.edu": {
+      "active_rooms": {
+        "1": 1
+      }
+    },
+    "jide@northwestern.edu": {
+      "active_rooms": {
+        "1": 1
+      }
+    },
+    "suzy@northwestern.edu": {
+      "active_rooms": {
+        "1": 1
+      }
+    }
+  }
+}
