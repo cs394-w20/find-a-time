0913fc86be515aefd54f74ca4eb3742a7470efa0
Zack <szaslan@gmail.com>
2020-01-27 11:11:14 -0600

    smol animation change

--- src/views/EventPage/EventPage.js
+++ src/views/EventPage/EventPage.js
@@ -1,40 +1,36 @@
-
-import React, {useContext, useEffect, useState} from "react"
+import React, { useContext, useEffect } from "react"
 import { AuthButton } from "components/AuthButton"
 import { Event } from "components/Event"
 import Calendar from "calendar/Calendar"
-import {AddUserToRoom} from "../../components/Db";
-import {normalEmailToFirebaseEmail} from "../../components/Utility";
+import { AddUserToRoom } from "../../components/Db"
+import { normalEmailToFirebaseEmail } from "../../components/Utility"
 import { UserContext } from "../../context/UserContext"
 
 /**
  * Get roomId from the path
  * @returns {string} the roomId taken from the path
  */
-const getRoomIdFromPath = () =>{
-    return window.location.pathname.split("/")[2];
-};
+const getRoomIdFromPath = () => {
+  return window.location.pathname.split("/")[2]
+}
 
 const EventPage = () => {
+  const userContext = useContext(UserContext)
 
-    const userContext = useContext(UserContext);
-
-    /**
-     * Adds user to room once logged in and on an EventPage by saving the email and profile pic in the roomId
-     * ToDo: ideally in the calendar code we can show names and the associated pics of people available.
-     */
-    useEffect( ()=>{
-            if (userContext.isUserLoaded){
-                AddUserToRoom(
-                    {
-                        roomId:getRoomIdFromPath(),
-                        email: normalEmailToFirebaseEmail(userContext.user.email),
-                        userName: userContext.user.name,
-                        picture: userContext.user.picture
-                    })
-            }
-        }, [userContext.isUserLoaded]
-    );
+  /**
+   * Adds user to room once logged in and on an EventPage by saving the email and profile pic in the roomId
+   * ToDo: ideally in the calendar code we can show names and the associated pics of people available.
+   */
+  useEffect(() => {
+    if (userContext.isUserLoaded) {
+      AddUserToRoom({
+        roomId: getRoomIdFromPath(),
+        email: normalEmailToFirebaseEmail(userContext.user.email),
+        userName: userContext.user.name,
+        picture: userContext.user.picture
+      })
+    }
+  }, [userContext.isUserLoaded])
 
   return (
     <div>
@@ -46,6 +42,6 @@ const EventPage = () => {
       <Calendar />
     </div>
   )
-};
+}
 
 export default EventPage
