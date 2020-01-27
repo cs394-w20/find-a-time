20d52515a3cb304059cf7d675aefb8ce4dbba83d
Zack <szaslan@gmail.com>
2020-01-26 23:25:18 -0600

    added animation delay lol

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -12,13 +12,17 @@ const Landing = () => {
       (Go to our test <Link to="events/1">event</Link>)
       <div className="landing__header-first-section-container">
         <div className="landing__header-text-container">
-          <div className="landing__header-text--bold">
+          <div className="landing__header-text--bold landing__header-delay">
             Find a time that works for <i>everyone</i>.
           </div>
-          <div className="landing__header-text--regular">
+          <div className="landing__header-text--regular landing__header-delay-1">
             Keep the scheduling simple and save the brain power for the meeting.
           </div>
-          <Link style={{ color: "white" }} to="/create">
+          <Link
+            style={{ color: "white" }}
+            to="/create"
+            className="landing__header-delay-2"
+          >
             <Button
               className="landing__header-cta"
               type="cta"
@@ -27,7 +31,11 @@ const Landing = () => {
             />
           </Link>
         </div>
-        <img src={landingImage} alt="People scheduling a meeting" />
+        <img
+          src={landingImage}
+          alt="People scheduling a meeting"
+          className="landing__header-delay-1"
+        />
       </div>
     </div>
   )
