ee4fc6dcdd03430e1e5cd95f59bfd379ae88014c
Jidé Anene <dominicanene@gmail.com>
2020-02-04 21:29:52 -0600

    Implemented changes s.t when GetRoomsByUser is implemneted YourEvents will work -Removed the dummy data from YourEvents and put it as return from GetRoomsByUser-Added more animations to YourEvents

--- src/components/GetRoomsByUser/FilterOldRooms.js
+++ src/components/GetRoomsByUser/FilterOldRooms.js
@@ -2,18 +2,21 @@ import db from "../Db/firebaseConnect";
 import moment from "moment";
 import {DATE_FORMAT} from "../../constants";
 
-
+/**
+ * Removes old rooms from the Db
+ */
 const removeRooms = async ({oldRooms,email})=>{
     const _removeRoom = (roomId) =>{
         db.ref().transaction(()=>{
-            // remove entry from the /users
-            db.ref('/users/'+email+'/active_rooms/'+roomId).remove();
-            // remove entry from the /rooms
-            db.ref('/rooms/'+roomId).remove()
+                // remove entry from the /users
+                db.ref('/users/'+email+'/active_rooms/'+roomId).remove();
+                // remove entry from the /rooms
+                db.ref('/rooms/'+roomId).remove()
             }
-        );
+        ).catch((e)=>console.error("Error in deleting a room",e));
     };
-    oldRooms.forEach((room)=>_removeRoom(room.id));
+
+    oldRooms.forEach((room)=>_removeRoom(room.roomId));
 };
 
 /**
@@ -21,11 +24,11 @@ const removeRooms = async ({oldRooms,email})=>{
  * and launches a async function in the background to delete the old
  * rooms
  * @param email - email address of the user in question
- * @param rooms - a list of rooms from dB each room needs to have
- *                a extra parameter called "id" which holds the "id"
- *                if you get data from Db this "id" is automatically exluced
- *                so must add this parameter manually, eg room["id"]=1, for
- *                each room. 
+ * @param rooms - a list of rooms from dB each room. Note each room needs to have
+ *                a extra parameter called "roomId" which holds the roomId.
+ *                If you get data from Db this "roomId" is automatically excluded
+ *                so must add this parameter manually, eg room["roomId"]=1, for
+ *                each room.
  * @return list of current rooms
  */
 const FilterOldRooms = ({email,rooms}) =>{
@@ -46,7 +49,7 @@ const FilterOldRooms = ({email,rooms}) =>{
     };
 
     let currentRooms = rooms.reduce(_filterRooms,[]);
-    // async remove of old rooms
+    // async remove of old rooms -- don't care about promise
     removeRooms({email,oldRooms});
     return currentRooms;
 
--- src/components/GetRoomsByUser/GetRoomsByUser.js
+++ src/components/GetRoomsByUser/GetRoomsByUser.js
@@ -3,7 +3,95 @@ import 'firebase/database';
 import 'firebase/auth';
 import db from "../Db/firebaseConnect";
 
-const GetRoomsByUser = async (user)=>{
+const sampleRoom1 = {
+    "roomId": 1,
+    "users": {
+        "suzy@northwestern_edu": {
+            "name": "Suzy",
+            "picture": "https://i.pinimg.com/originals/f0/03/44/f00344d904062ce92b4b3b146060d874.png"
+        },
+        "jide@northwestern_edu": {
+            "name": "Jidé",
+            "picture": "https://i.pinimg.com/originals/f0/03/44/f00344d904062ce92b4b3b146060d874.png"
+        },
+        "julia@northwestern_edu": {
+            "name": "Julia",
+            "picture": "https://i.pinimg.com/originals/f0/03/44/f00344d904062ce92b4b3b146060d874.png"
+        }
+    },
+    "time_interval": {
+        "start": "2020-01-20",
+        "end": "2020-01-22"
+    },
+    "meta_data": {
+        "title": "CS 396 Project v3",
+        "description": "Make sure you fill out this form so we can find a time to meet weekly!",
+        "room_owner": "suzy@northwestern_edu"
+    }
+};
+
+const sampleRoom2 = {
+    "roomId": 19,
+    "users": {
+        "suzy@northwestern_edu": {
+            "name": "Suzy",
+            "picture": "https://i.pinimg.com/originals/f0/03/44/f00344d904062ce92b4b3b146060d874.png"
+        }
+    },
+    "time_interval": {
+        "start": "2020-01-10",
+        "end": "2020-01-12"
+    },
+    "meta_data": {
+        "title": "CS 394 Meeting",
+        "description": "Make sure you fill out this form so we can find a time to meet weekly!",
+        "room_owner": "suzy@northwestern_edu"
+    }
+};
+
+const sampleRoom3 = {
+    "roomId": 20,
+    "users": {
+        "suzy@northwestern_edu": {
+            "name": "Suzy",
+            "picture": "https://i.pinimg.com/originals/f0/03/44/f00344d904062ce92b4b3b146060d874.png"
+        }
+    },
+    "time_interval": {
+        "start": "2020-02-10",
+        "end": "2020-02-12"
+    },
+    "meta_data": {
+        "title": "Chess Club meeting",
+        "description": "Make sure you fill out this form so we can find a time to meet weekly!",
+        "room_owner": "suzy@northwestern_edu"
+    }
+};
+
+// ReactSearchBox `searches' on value, and the payload is key.
+const sampleData = [
+    {
+        key: sampleRoom1,
+        value: 'CS 396 Project v3',
+    },
+    {
+        key: sampleRoom2,
+        value: 'CS 394 Meeting',
+    },
+    {
+        key: sampleRoom3,
+        value: 'Chess Club meeting',
+    }
+];
+
+
+const getRoom = async ({roomId})=>{
+    return await db.ref('rooms/'+roomId).once('value').val();
+};
+
+const GetRoomsByUser = async ({email})=>{
+
+
     let roomlist =null;
     let userrooms =null;
     const process = (snapshot) =>{
@@ -13,13 +101,15 @@ const GetRoomsByUser = async (user)=>{
     const process2 = (snapshot) =>{
         userrooms = snapshot.val()||{}
     };
-    await db.ref('/users' + user+"/active_rooms/").once('value',process2);
+    await db.ref('/users/' + email+"/active_rooms/").once('value',process2);
     console.log(roomlist);
     console.log(userrooms);
     /* var result = [];
     for(var i in roomlist)
         result[i] = roomlist[i];
     console.log(result); */
-    return roomlist;
+    //return roomlist;
+
+    return sampleData
 };
 export default GetRoomsByUser;
