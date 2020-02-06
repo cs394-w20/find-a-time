9372c0808495b78840a832bf0d625e11d138972e
Jidé Anene <dominicanene@gmail.com>
2020-02-06 17:25:23 -0600

    EventPage won't execute if room is not in Db - [AddUserToRoom] is now a transaction it checks if room exists b4 doing anything - new function in Db folder [hasRoom] checks if rooms exists in Db and returns a boolean - I added a state boolean variable to [EventPage] called dbHasRoom everything waits until this is set to true. However, things are still broken@!!!! I tried going to a room thaat exists and the code pull all my event data back from when i first created my google account. I've been debuggin and its because [GetStartEndTimeForRoomId] which is called in [ListUpcomingEvents] get a null variable instead of the id. On the [EventPage] the id may not be getting passed in or something else funny is happening but [GetStartEndTimeForRoomId] does not get data and the roomId is null i know that

--- src/components/Db/AddUserToRoom.js
+++ src/components/Db/AddUserToRoom.js
@@ -2,14 +2,21 @@ import 'firebase/database';
 import 'firebase/auth';
 import db from "./firebaseConnect";
 
+// Adds user to room iff the room exists
 const AddUserToRoom = async ({email, userName, roomId, picture}) =>{
-        await db.ref('rooms/' + roomId + "/users/"+email +"/")
-            .set({name:userName,picture:picture})
-            .catch(error => alert(error));
-        
-        await db.ref('users/' + email + "/active_rooms/"+roomId+"/")
-            .set(1)
-            .catch(error => alert(error));
+        let dbRef = db.ref('rooms/' + roomId);
+
+        await dbRef.transaction(async (room) => {
+                if (room !== null) {
+                        db.ref('rooms/' + roomId + "/users/" + email + "/")
+                            .set({name: userName, picture: picture})
+                            .catch(error => alert(error));
+
+                        db.ref('users/' + email + "/active_rooms/" + roomId + "/")
+                            .set(1)
+                            .catch(error => alert(error));
+                }
+        });
 
 };
 
--- src/components/Db/index.js
+++ src/components/Db/index.js
@@ -4,4 +4,5 @@ export {default as UpdateDb} from './UpdateDb';
 export {default as db} from './firebaseConnect';
 export {default as GetStartEndTimeForRoomId} from './GetStartEndTimeForRoomId';
 export {default as AddUserToRoom} from './AddUserToRoom';
+export {default as hasRoom} from './hasRoom';
 
